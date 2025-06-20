d158cff8223d8aa00c8d5ca7e41befbce033be84
leofevife <leofevife@gmail.com>
2025-06-20 08:48:15 -0300

    Primeiro Commit

---
+++ MinhaVaga/src/testes/Locatario.java
@@ -0,0 +1,7 @@
+package testes;
+
+public class Locatario extends Usuario {
+	
+	private String dataPagamento;
+	private String metodoPagamento;
+}
---
+++ MinhaVaga/src/testes/Usuario.java
@@ -0,0 +1,10 @@
+package testes;
+
+public class Usuario {
+
+	private String nome;
+	private String cpf;
+	private String numCel;
+	private String endereco;
+	
+}
